2f1e98726248a10e211457ee476463a200be1c2d
Betty Lin <bettylin0823@hotmail.com>
2018-11-19 21:10:17 +0800

    add US part

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
-country = input('你所在的國家為: ')
-age = input('請輸入你的年齡: ')
+country = input('你所在的國家為/Please enter your country: ')
+age = input('請輸入你的年齡/Please enter your age: ')
 age = int(age)
-if country == 'Taiwan' or '台灣' or '臺灣':
+if country == '台灣':
 	if age >= 18:
 		print('你可以考駕照')
 	else:
 		print('你還不能考駕照')
+elif country == '美國':
+	if age >= 16:
+		print('You can get the drivers licenses.')
+	else:
+		print('You are not old enough to drive.')
